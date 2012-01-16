c82fd3533f1060604b8fa3ccf7806ff464932a98
Patrick Brosset <patrick.brosset@amadeus.com>
2012-01-16 22:32:43 +0100

    Put in place templating and output of report
    USing the django templates to output the pages.
    Also, not using the default reporter anymore, now
    creating my own report using the template

--- main.py
+++ main.py
@@ -2,65 +2,23 @@ import os
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
+from google.appengine.ext.webapp import template
 
 from cleanjs import fileparser
 from cleanjs.messagebag import MessageBag
 from cleanjs.reviewers import codesize, comments, complexity, formatting, naming, unused, general
-from cleanjs.reporters import htmlwithcode
 
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
-	def get(self):
-		content = """
-		<!DOCTYPE html>
-		<html lang="en">
-			<head>
-				<meta charset="utf-8">
-			    <title>cleanjs report for file testscripts/goodfile.js</title>
-				<style type="text/css">
-					body {
-						margin: 0;
-						padding: 1em;
-						font-size: 14px;
-						font-family: 'Palatino Linotype', 'Book Antiqua', Palatino, FreeSerif, serif;
-						overflow-x: hidden;
-						width: 100%;
-					}
-					h1 {
-						margin: 0;
-						padding: 1em 1em 1em 25px;
-					}
-					p {
-						margin: 0;
-						padding: 1em 1em 1em 25px;
-					}
-					textarea {
-						width: 500px;
-						height: 500px;
-					}
-					button {
-						font-size: 20px;
-						font-weight: bold;
-					}
-				</style>
-			</head>
-			<body>
-				<a href="http://github.com/captainbrosset/cleanjs"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://a248.e.akamai.net/assets.github.com/img/e6bef7a091f5f3138b8cd40bc3e114258dd68ddf/687474703a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub"></a>
-				<h1>cleanjs</h1>
-				<p>Copy your javascript source code below and hit 'review'</p>
-				<form method="post" action="/">
-					<p>
-						<textarea name="code"></textarea>
-					</p>
-					<p>
-						<button type="submit">review</button>
-					</p>
-				</form>
-			</body>
-		</html>"""
+class BaseHandler(webapp2.RequestHandler):
+	def writeTemplateToResponse(self, tpl, data):
+		path = os.path.join(os.path.dirname(__file__), "templates" + os.sep + tpl)
 		self.response.headers['Content-Type'] = 'text/html'
-		self.response.out.write(content)
+		self.response.out.write(template.render(path, data))
+
+class MainPage(BaseHandler):
+	def get(self):
+		self.writeTemplateToResponse("index.html", {})
 		
 	def post(self):
 		file_data = fileparser.get_file_data_from_content("cleanjs", self.request.get('code'))
@@ -74,10 +32,13 @@ class MainPage(webapp2.RequestHandler):
 		formatting.Reviewer().review(file_data, message_bag)
 		naming.Reviewer().review(file_data, message_bag)
 		unused.Reviewer().review(file_data, message_bag)
-		
-		content = htmlwithcode.output_messages(message_bag, file_data)
-		
-		self.response.headers['Content-Type'] = 'text/html'
-		self.response.out.write(content)
+	
+		lines = file_data.lines.all_lines
+		lines_data = []
+		for line in lines:
+			messages = message_bag.get_messages_on_line(line.line_number)
+			lines_data.append({"messages": messages, "message_count": len(messages), "line": line})
+	
+		self.writeTemplateToResponse("report.html", {"bag" : message_bag, "lines" : lines_data})
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
